30b099b4bc4070599076e4d96b0d7d3fa82e4c63
itmoti <motiarrahman9707@gmail.com>
2023-06-15 22:21:41 +0600

    testing

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,11 @@
-import express, { Application, Request, Response } from "express";
-const app: Application = express();
-const port = 3000;
-import cors from "cors";
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.get("/", (req: Request, res: Response) => {
-  res.send("website is running ");
-});
+import express, { Application, Request, Response } from 'express'
+const app: Application = express()
+import cors from 'cors'
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.get('/', (req: Request, res: Response) => {
+  res.send('website is running ')
+})
 
-export default app;
+export default app
--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,16 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./config";
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config'
 function bootstrap() {
-
-    try {
-        mongoose.connect(config.database_url as string);
-        console.log('connected')
-        app.listen(config.port, () => {
-            console.log(`Application listening on port ${config.port}`)
-          })
-    }
-    catch (err) {
-
-    }
-   
+  try {
+    mongoose.connect(config.database_url as string)
+    console.log('connected')
+    app.listen(config.port, () => {
+      console.log(`Application listening on port ${config.port}`)
+    })
+  } catch (err) {
+    /* empty */
+  }
 }
 
 bootstrap()
